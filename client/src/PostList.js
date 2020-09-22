b41f0e00e4f2941c4ed991355f105e02cc5275bc
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-23 01:32:18 +0530

    used query service to retrive posts and comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,9 +7,9 @@ export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts');
-    console.log(res.data.posts);
-    setPosts(res.data.posts);
+    const res = await axios.get('http://localhost:4002/posts');
+    console.log(res.data);
+    setPosts(res.data);
   };
 
   useEffect(() => {
@@ -25,7 +25,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
