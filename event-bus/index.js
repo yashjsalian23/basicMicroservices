5bb2b1786288ec4f6d83940bdca6f66c441687d4
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-23 19:57:00 +0530

    event storing feature added

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,22 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+let events = [];
+
 app.post("/events", (req, res) => {
     const event = req.body;
-
+    events.push(event);
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
     axios.post('http://localhost:4003/events', event);
 
     res.send({status: "OK"}); 
-})
+});
+
+app.get("/events", (res, req) => {
+    res.send(events);
+});
 
 app.listen(4005, () => {
     console.log('4005 server started');
