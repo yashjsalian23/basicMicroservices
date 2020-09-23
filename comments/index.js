f057f7ab808b4d06f3d779e3027efed34082a8b4
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-23 15:49:20 +0530

    updated comment status based on moderation

--- comments/index.js
+++ comments/index.js
@@ -39,6 +39,27 @@ app.post("/posts/:id/comments", (req, res) => {
 
 app.post("/events", (req, res) => {
     console.log('event recived',req.body.type);
+
+    const { type, data } = req.body;
+
+    if(type === 'CommentModerated'){
+        const { id, postId, content, status } = data;
+
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(c => c.id === id);
+        comment.status = status;
+    
+        axios.post("http://localhost:4005/events",{
+            type: "ComentUpdated",
+            data: {
+                id,
+                postId,
+                content,
+                status
+            }
+        });
+    }
+
     res.send({});
 });
 
