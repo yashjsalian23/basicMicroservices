be460241bfdba71a4fde0f954fb675211c32d72e
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-23 17:17:03 +0530

    rendered comment by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 export default ({ comments }) => {
   
-
+  let content;
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    
+    if(comment.status === 'approved'){
+      content = comment.content;
+    }
+
+    if(comment.status === 'rejected'){
+      content = 'This comment is rejected';
+    }
+
+    if(comment.status === 'pending'){
+      content = 'This comment is awaiting moderation';
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
